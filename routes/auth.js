0095d5a72b87e6f5c6445e6db169f83d86e53beb
chinnasivakrishna <chinnasivakrishna2003@gmail.com>
2025-04-23 15:13:08 +0530

    done

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,7 @@ const { protect } = require('../middleware/auth');
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:5000/api/auth/google/callback"
+  callbackURL: "https://auriter-backen.onrender.com/api/auth/google/callback"
 },
 async (accessToken, refreshToken, profile, done) => {
   try {
